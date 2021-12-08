7020e89bd06500fdbd64d52ab9c05d46907e2019
HejBajs <92580760+HejBajs@users.noreply.github.com>
2021-12-08 12:20:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function draw() {
     buttons.update();
     fill(255);
     textSize(50);
-    text("$"+score, width/2, 60);
+    text("$"+nFormatter(score, 1), width/2, 50);
 }
 
 function collide(x, y, w, h, x2, y2){
@@ -82,4 +82,21 @@ function isMouseInside(x, y, w, h){
     } else {
         return false; 
     }
+}
+
+function nFormatter(num, digits) {
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var item = lookup.slice().reverse().find(function(item) {
+        return num >= item.value;
+    });
+    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
 }
