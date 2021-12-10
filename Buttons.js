ea941f97332051ae4bed74aeaac0139dfce34601
HejBajs <92580760+HejBajs@users.noreply.github.com>
2021-12-10 11:39:14 +0100

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -1,35 +1,57 @@
-class Falling{
+class Buttons{
     constructor(){
-        this.y = random(-10, -1000);
-        this.x = random(float(0), float(windowWidth));
-        this.c = color(random(255), random(255), random(255));
-        this.size = (30);
-        this.speed = 5;
-        this.specint = int(random(1, 1000));
-        this.special = false;
-        this.specialRand();
+        this.c = color(80);
+        this.plus = 10;
+        this.cost1 = 10;
+        this.cost2 = 10;
+        this.cost3 = 10;
+        this.cost4 = 1000000;
+        this.cost5 = 1000000;
+        this.hej = true;
+        this.hej2 = true;
+        this.d = this.nFormatter(this.cost4);
+        this.d2 = this.nFormatter(this.cost5);
     }
     
     update(){
-        this.draw();
-    }
-    
-    draw(){
+        if(this.hej == false){
+            this.d = "max";
+        }
+        
+        if(this.hej2 == false){
+            this.d2 = "max";
+        } else{
+            this.d2 = this.nFormatter(this.cost5);
+        }
         fill(this.c);
         noStroke();
-        ellipse(this.x, this.y, this.size);
-        
-        this.y += this.speed;
+        rect(10, 10, 140, 50, 10);
+        rect(200, 10, 140, 50, 10);
+        rect(390, 10, 140, 50, 10);
+        rect(width-210, 10, 200, 50, 10);
+        rect(width-420, 10, 200, 50, 10);
+        fill(255);
+        textSize(20);
+        text("Balls+ $"+this.nFormatter(this.cost1), 20, 40);
+        text("money+ $"+this.nFormatter(this.cost2), 210, 40);
+        text("size+ $"+this.nFormatter(this.cost3), 400, 40);
+        text("speed+ $"+this.d, width-185, 40);
+        text("special " + chance/10 + "% " + this.d2, width-395, 40);
     }
-    
-    specialRand(){
-        for(var i = 1; i < chance+1; i++){
-            if(this.specint == i){
-                this.special = true;
-                this.size+=10;
-                this.c = color(255,215,0);
-                this.speed++;
-            }
-        }
+    nFormatter(num) {
+      const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "B" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+      ];
+      const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+      var item = lookup.slice().reverse().find(function(item) {
+        return num >= item.value;
+      });
+      return item ? (num / item.value).toFixed(1).replace(rx, "$1") + item.symbol : "0";
     }
 }
