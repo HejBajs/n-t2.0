7572f0bee7a575d18a238f9c9be3213233cb261d
HejBajs <92580760+HejBajs@users.noreply.github.com>
2021-12-20 10:55:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,24 @@ let botmode = false;
 let cheatCode = ['n', 'a', 'l', 'l', 'e', 'p', 'u'];
 let keyl = [];
 
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)){
+    return false;
+    }
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     buttons = new Buttons();
